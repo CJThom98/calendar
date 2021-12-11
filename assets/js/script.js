85ea58bfc9819ff2bef761a209de967b44df8c70
Cory Thompson <cory.thompson98@gmail.com>
2021-12-11 08:25:53 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,5 @@ var date = (today.getMonth()+1)+"/"+today.getDate()+"/"+today.getFullYear();
 var currentDate = "Today's Date is: " + date;
 
 document.getElementById("currentDay").innerHTML = currentDate;
+
+var tasks = {};
