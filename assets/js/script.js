82e987b83811f7676a0ea7edc5d49c4230eda0e7
Cory Thompson <cory.thompson98@gmail.com>
2022-01-29 16:05:51 -0600

    second try

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,12 @@ var date = (today.getMonth()+1)+"/"+today.getDate()+"/"+today.getFullYear();
 var currentDate = "Today's Date is: " + date;
 
 document.getElementById("currentDay").innerHTML = currentDate;
+
+var newTime = new Date();
+var thisHour = newTime.getHours();
+console.log(thisHour);
+
+var loadEvents = function () {
+    var event9 = (localStorage.getItem("event9"));
+    var event10 = (localStorage.getItem("event10"));
+}
