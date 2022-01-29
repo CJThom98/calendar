278774b6c2b86aad37608778108e079344dddf48
Cory Thompson <cory.thompson98@gmail.com>
2022-01-29 16:09:10 -0600

    other events

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,10 @@ var loadEvents = function () {
     var event9 = (localStorage.getItem("event9"));
     var event10 = (localStorage.getItem("event10"));
     var event11 = (localStorage.getItem("event11"));
+    var event12 = (localStorage.getItem("event12"));
+    var event13 = (localStorage.getItem("event13"));
+    var event14 = (localStorage.getItem("event14"));
+    var event15 = (localStorage.getItem("event15"));
+    var event16 = (localStorage.getItem("event16"));
+    var event17 = (localStorage.getItem("event17"));
 }
